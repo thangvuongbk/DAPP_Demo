0ea559529afa5cd916a2cf26224478bbbebcb3a5
Vuong Van Thang <thangvuongbk@gmail.com>
2019-08-25 15:46:17 +0700

    DAPP Demo

---
+++ truffle-config.js
@@ -0,0 +1,30 @@
+//var HDWalletProvider = require("truffle-hdwallet-provider");
+//const MNEMONIC = 'EE755A0B796BE0992994FE197911624AD98B34A89A81CBEF4988E0569C440874';
+
+var PrivateKeyProvider = require("truffle-privatekey-provider");
+var privateKey = 'EE755A0B796BE0992994FE197911624AD98B34A89A81CBEF4988E0569C440874';
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {    
+    development: {
+      host: "127.0.0.1",
+      port: "7545",
+      network_id: "*" // match any network id
+    },    
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: 4,
+      gas: 4700000
+    },       
+    ropsten: {
+      provider: function() {
+        return new PrivateKeyProvider(privateKey, "https://ropsten.infura.io/v3/c567e6deb0c74854b5b0b83758d3f09a")
+      },
+      network_id: 3,
+      gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
+    }
+  }
+};
